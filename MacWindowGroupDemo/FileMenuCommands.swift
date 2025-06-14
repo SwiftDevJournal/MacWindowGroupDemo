427acadab01c85592c7be0b09985273346c754e2
Swift Dev Jounal <mark@swiftdevjournal.com>
2025-06-14 14:26:18 -0400

    Add the code to handle the Open menu item in the File menu.

--- MacWindowGroupDemo/FileMenuCommands.swift
+++ MacWindowGroupDemo/FileMenuCommands.swift
@@ -5,4 +5,28 @@
 //  Created by mark on 6/14/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct OpenFolderView: View {
+    @State private var shouldOpenFolder = false
+    @Environment(\.openWindow) private var openWindow
+    
+    var body: some View {
+        Group {
+            Button(action: {
+                shouldOpenFolder = true
+            }, label: {
+                Text("Open Folder")
+            }).keyboardShortcut("O", modifiers: [.command])
+        }
+        .fileImporter(isPresented: $shouldOpenFolder, allowedContentTypes: [.folder], onCompletion: { result in
+            switch result {
+            case .success(let url):
+                let folder = Folder(url: url)
+                openWindow(id: "repo", value: folder)
+            case .failure(let error):
+                print(error)
+            }
+        })
+    }
+}
