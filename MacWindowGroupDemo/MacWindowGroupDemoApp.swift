6800d8ef512a6461d8a4af251f975771fa3d2b3e
Swift Dev Jounal <mark@swiftdevjournal.com>
2025-06-14 14:22:20 -0400

    Initial Commit

---
+++ MacWindowGroupDemo/MacWindowGroupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacWindowGroupDemoApp.swift
+//  MacWindowGroupDemo
+//
+//  Created by mark on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MacWindowGroupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacWindowGroupDemoTests/MacWindowGroupDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacWindowGroupDemoTests.swift
+//  MacWindowGroupDemoTests
+//
+//  Created by mark on 6/14/25.
+//
+
+import Testing
+@testable import MacWindowGroupDemo
+
+struct MacWindowGroupDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
